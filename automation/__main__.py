3f2861f9b4eedcf90d6971e42524410d360cc21d
Evgeny Feigelman <namlegief@gmail.com>
2025-05-03 22:16:15 +0300

    fix update_index_html and  setup_template.py

---
+++ automation/__main__.py
@@ -0,0 +1,5 @@
+from automation.setup_template import main
+
+if __name__ == "__main__":
+    main()
+
--- automation/setup_template.py
+++ automation/setup_template.py
@@ -4,23 +4,23 @@ from automation.unzip_and_move import unzip_and_move
 from automation.update_index_html import update_index_links
 
 def main():
-    print("📦 Step 1: Download template")
+    print(" Step 1: Download template")
     archive = download_template()
     if not archive:
-        print("❌ Aborting: Failed to download template.")
+        print(" Aborting: Failed to download template.")
         return
 
-    print("\n🗂️ Step 2: Unzip and move files")
+    print("\n Step 2: Unzip and move files")
     unzip_and_move(archive)
 
     index_path = os.path.join("src", "details", "templates", "index.html")
     if os.path.exists(index_path):
-        print("\n🛠️ Step 3: Update index.html paths")
+        print("\n Step 3: Update index.html paths")
         update_index_links()
     else:
-        print(f"\n⚠️ Skipping Step 3: index.html not found at {os.path.abspath(index_path)}")
+        print(f"\n Skipping Step 3: index.html not found at {os.path.abspath(index_path)}")
 
-    print("\n🎉 Done!")
+    print("\n Done!")
 
 if __name__ == "__main__":
     main()
